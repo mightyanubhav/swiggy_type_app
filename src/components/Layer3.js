9ee304e8cd97be02b70857f97387304f378235ec
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-11-09 09:27:31 +0530

    layer3 done

--- src/components/Layer3.js
+++ src/components/Layer3.js
@@ -1,7 +1,21 @@
+import Card from "./Card";
 const Layer3 = ({data}) => {
     return (
         <div className="layer3">
-            <div></div>
+            <div className="layer3-top">
+                Restaurants with online food delivery in Jamshedpur
+            </div>
+            <div className="layer3-options">
+                <button className="layer3-filter-btn">filter</button>
+            </div>
+            <div className="layer3-cards">
+                {
+                    data.map((x) => (
+                        <Card key={x.info.id} name = {x.info.name} image = {x.info.cloudinaryImageId} area = {x.info.areaName} rating = {x.info.avgRating} time = {x.info.sla.slaString} menu = {x.info.cuisines} items = {x.info.aggregatedDiscountInfoV3}/>
+                    ))
+                }
+            </div>
+
         </div>
     );
 }
