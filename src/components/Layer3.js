4dcfabbea46a56334b7c9fc4795d596d82f54c55
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-11-11 10:25:25 +0530

    first commit

--- src/components/Layer3.js
+++ src/components/Layer3.js
@@ -1,17 +1,32 @@
+import { useState } from "react";
 import Card from "./Card";
-const Layer3 = ({data}) => {
+const Layer3 = ({ data }) => {
+
+    console.log(data);
+    const [listOfRestaurant, setListOfRestaurant] = useState(data);
+    // const [a, b] = useState(data);
+
+    // const arr = useState(data);
+    // const [c, d] = arr; it is array dstructuring
     return (
         <div className="layer3">
             <div className="layer3-top">
                 Restaurants with online food delivery in Jamshedpur
             </div>
             <div className="layer3-options">
-                <button className="layer3-filter-btn">filter</button>
+                <button className="layer3-filter-btn" onClick={
+                    () => {
+                        const filter = listOfRestaurant.filter(
+                            (x) => x.info.avgRating > 4.2
+                        )
+                        setListOfRestaurant(filter);
+                    }
+                }>filter</button>
             </div>
             <div className="layer3-cards">
                 {
-                    data.map((x) => (
-                        <Card key={x.info.id} name = {x.info.name} image = {x.info.cloudinaryImageId} area = {x.info.areaName} rating = {x.info.avgRating} time = {x.info.sla.slaString} menu = {x.info.cuisines} items = {x.info.aggregatedDiscountInfoV3}/>
+                    listOfRestaurant.map((x) => (
+                        <Card key={x.info.id} name={x.info.name} image={x.info.cloudinaryImageId} area={x.info.areaName} rating={x.info.avgRating} time={x.info.sla.slaString} menu={x.info.cuisines} items={x.info.aggregatedDiscountInfoV3} />
                     ))
                 }
             </div>
