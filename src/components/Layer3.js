e010ecbff353c40b4762f729af27f0a967dfbb44
Anubhav Shukla <kumaranubhav691@gmail.com>
2024-11-12 13:32:44 +0530

    search bar put

--- src/components/Layer3.js
+++ src/components/Layer3.js
@@ -2,11 +2,12 @@ import { useEffect, useState } from "react";
 import Card from "./Card";
 import getData from "./RandomFood";
 import Shimmer from "./Shimmer";
+import { url2 } from "../utils/Constant";
+
 const Layer3 = ({ data }) => {
-
     const [listOfRestaurant, setListOfRestaurant] = useState([]);
-
-    const url2 = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=22.80570&lng=86.18040&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
+    const [log, setLog] = useState("login");
+    const [searchText, setSearchText] = useState("");
 
     useEffect(() => {
         fetchRestaurants();
@@ -21,36 +22,70 @@ const Layer3 = ({ data }) => {
         }
     };
 
-    //conditional rendering
-    // let log = "login";
-    const [log, setLog] = useState("login")
-    return listOfRestaurant.length === 0 ? <Shimmer /> : (
+    return listOfRestaurant.length === 0 ? (
+        <Shimmer />
+    ) : (
         <div className="layer3">
             <div className="layer3-top">
                 Restaurants with online food delivery in Jamshedpur
             </div>
             <div className="layer3-options">
-                <button className="layer3-filter-btn" onClick={
-                    () => {
+                <button
+                    className="layer3-filter-btn"
+                    onClick={() => {
                         const filter = listOfRestaurant.filter(
                             (x) => x.info.avgRating > 4.2
-                        )
+                        );
                         setListOfRestaurant(filter);
-                    }
-                }>filter</button>
-                 <button className="layer3-filter-btn" onClick={()=>{
-                    log === "login" ? setLog("log-out") : setLog("login");
-                 }}> {log} </button>
+                    }}
+                >
+                    filter
+                </button>
+                <button
+                    className="layer3-filter-btn"
+                    onClick={() => {
+                        log === "login" ? setLog("log-out") : setLog("login");
+                    }}
+                >
+                    {" "}
+                    {log}{" "}
+                </button>
+
+                <div className="search-bar">
+                    <input
+                        type="text"
+                        className="input-box"
+                        placeholder="Search restaurant"
+                        value={searchText}
+                        onChange={(e) => {
+                            setSearchText(e.target.value);
+                        }}
+                    />
+                    <button
+                        className="layer3-filter-btn"
+                        onClick={() => {
+                            console.log(searchText);
+                        }}
+                    >
+                        Search
+                    </button>
+                </div>
             </div>
             <div className="layer3-cards">
-                {
-                    listOfRestaurant.map((x) => (
-                        <Card key={x.info.id} name={x.info.name} image={x.info.cloudinaryImageId} area={x.info.areaName} rating={x.info.avgRating} time={x.info.sla.slaString} menu={x.info.cuisines} items={x.info.aggregatedDiscountInfoV3} />
-                    ))
-                }
+                {listOfRestaurant.map((x) => (
+                    <Card
+                        key={x.info.id}
+                        name={x.info.name}
+                        image={x.info.cloudinaryImageId}
+                        area={x.info.areaName}
+                        rating={x.info.avgRating}
+                        time={x.info.sla.slaString}
+                        menu={x.info.cuisines}
+                        items={x.info.aggregatedDiscountInfoV3}
+                    />
+                ))}
             </div>
-
         </div>
     );
-}
+};
 export default Layer3;
--- src/components/RandomFood.js
+++ src/components/RandomFood.js
@@ -16,7 +16,6 @@ async function getData(url){
     }
     
 }
-const url = "https://www.swiggy.com/dapi/restaurants/list/v5?lat=26.14860&lng=85.89730&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING";
-// getData(url);
+
 
 export default getData;
